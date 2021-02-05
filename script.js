efb6e9c3d3592389dc6441479ac912bc45d8c655
Avgurin <58801442+Avgurin@users.noreply.github.com>
2021-02-05 23:04:08 +1100

    rand

--- script.js
+++ script.js
@@ -1,83 +1,45 @@
-// const result = document.getElementById("current-result");
-// const btnLess = document.getElementById("btn_less");
-// const btnMore = document.getElementById("btn_more");
-// const btnEnd = document.getElementById("btn_end");
-// const endGame = document.getElementById("end-game");
-// const maxTry = document.getElementById("max-try");
-// const logTry = document.getElementById("log-try");
+
+
+//массив частй для ника
+let part = ["pro", "dreamsq", "tv", "333", 
+"mavrodi", "228", "mider", "top", "palifagus", 
+"UKR", "blackReaper", "StarHacker2", "СинийЛучНик", 
+"skeletikgames","misterL38956", "borislav", "TheDanilYT", "bel", "zachem-mne-spasat-otrebye-vrode-tebya",
+"KanekiKun", "movi", "fortnite", "YaroslavMirnyi", "Pro100_marik", "assasin", "blackDimaIQ228", "navalniyTOP", "SSS", "popov", "666"];
+
+//максимальный случайный член массива
+let part_max = part.length;
+console.log(part_max);
+
+//максимальная количество частей ника
+let max = 6;
+console.log(max);
+
+//минимальное количество частей ника
+let min = 3;
+console.log(min);
+
+//случайное число для количества частей ника
+let nick = Math.floor(Math.random() * (max - min + 1) ) + min;
+console.log(nick);
+
+//делаем ник
+let rand_w;
+let full_nick = "";
+let new_part;
+
+for (i=0; i <= nick; i++) {
+	do {
+		rand_w = Math.floor(Math.random() * Math.floor(part_max));
+		new_part = part[rand_w];
+	} while (full_nick.includes(new_part))
+
+	full_nick = full_nick+"_"+new_part;
+}
+console.log(full_nick);
+
 
 let name = prompt("Введи свое имя");
 const logName = document.getElementById("log-try");
-logName.textContent = name;
+logName.textContent = name+"_65rus"+full_nick;
 
-// let maxNum = parseInt(prompt("Загадайте число от 1 до ","100"));
-// let cheat = 0;
-// let logTryNum = 0;
-// let directionSymbol = "";
-// let tryCount = 0;
-
-// if (isNaN(maxNum) || maxNum < 1) {
-//     alert("Какую-то фигню ввели. Макс число будет 100");
-//     maxNum = 100;
-// }
-
-// maxNum +=1;
-// let currentNum = maxNum;
-// let jump = Math.round(currentNum / 2);
-// maxTry.textContent = Math.round(Math.log2(maxNum));
-// play (-1);
-
-// function play(direction) { 
-//     if ( 
-//         direction===-1 && jump<10 && currentNum-jump < 0 ||
-//         direction===1 && jump<10 && currentNum+jump >= maxNum){
-//         alert("Не может быть )");
-//     } else if (cheat == 2){
-//         end();
-//     } else {
-//         tryCount += 1;
-//         currentNum = currentNum + direction * jump;
-//         showResult(currentNum);
-//         directionSymbol = direction>0?" > ":" < ";
-//         logTryNum = jump===Math.round(maxNum / 2)?currentNum:(logTryNum + directionSymbol + currentNum);
-//         showLogTry(logTryNum);
-        
-//         jump=Math.round(jump / 2);
-//         if (jump === 1){cheat++};
-//         console.log(`cheat= ${cheat} currentNum=${currentNum} jump=${jump}`);
-//     }
-// }
-
-// function doLess (){
-//     play(-1);
-// }
-
-// function doMore (){
-//     play(1);
-// }
-
-// function doMore (){
-//     play(1);
-// }
-
-// function showResult(tryNum) {
-//     result.textContent = tryNum;
-// }
-
-// function showLogTry(Num) {
-//     logTry.textContent = Num;
-// }
-
-// function end() {
-//     endGame.textContent = "Потребовалось попыток: " + tryCount+ ". Было загадано число "+currentNum;
-//     btnLess.hidden = true;
-//     btnMore.hidden = true;
-//     btnEnd.hidden = true;
-//     document.getElementById("results").hidden = true;
-
-
-// }
-
-// btnLess.addEventListener ('click',doLess);
-// btnMore.addEventListener ('click',doMore);
-// btnEnd.addEventListener ('click',end);
